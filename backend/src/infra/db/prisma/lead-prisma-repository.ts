dc1da05e400e99df0efb90fe72110149ce095f87
Caique Degaspari <caique.degaspare@gmail.com>
2023-12-11 15:55:38 -0300

    feat: add add-lead infra layer

--- backend/src/infra/db/prisma/lead-prisma-repository.ts
+++ backend/src/infra/db/prisma/lead-prisma-repository.ts
@@ -2,6 +2,16 @@ import { AddLeadRepository } from '@/data/protocols/db/lead/add-lead-repository'
 import prisma from './client'
 
 export class LeadPrismaRepository implements AddLeadRepository {
-  async add (params: AddLeadRepository.Params): Promise<void> {
+  async add ({ unidades, ...params }: AddLeadRepository.Params): Promise<void> {
+    await prisma.lead.create({
+      data: {
+        ...params,
+        unidades: {
+          createMany: {
+            data: unidades
+          }
+        }
+      }
+    })
   };
 }
