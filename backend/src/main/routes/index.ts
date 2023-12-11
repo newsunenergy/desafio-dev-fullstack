87c2714b3e1e44419135ad71d4edc9d53643f792
Caique Degaspari <caique.degaspare@gmail.com>
2023-12-11 20:05:36 -0300

    feat: add load-lead-by-id presentation and main layer

--- backend/src/main/routes/index.ts
+++ backend/src/main/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { makeAddLeadController } from '../factories/controllers/add-lead-controller-factory'
+import { makeLoadLeadByIdController } from '../factories/controllers/load-lead-by-id-controller'
 
 export default (router: Router): void => {
   router.post('/lead', makeAddLeadController)
+  router.get('/lead', makeLoadLeadByIdController)
 }
