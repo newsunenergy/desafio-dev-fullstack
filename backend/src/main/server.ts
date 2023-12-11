9a193a75e6cc8ec5b9fd0fba8787f0e3046ca791
Caique Degaspari <caique.degaspare@gmail.com>
2023-12-11 08:39:41 -0300

    add initial backend structure

---
+++ backend/src/main/server.ts
@@ -0,0 +1,7 @@
+const bootstrap = async (): Promise<void> => {
+  const app = (await import('./config/app')).default
+  app.listen(4000, () => console.log('🌞 New Sun challenge running at http://localhost:4000 🌞'))
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap()
