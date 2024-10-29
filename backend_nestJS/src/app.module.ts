cb3344994e08ac22cd8bcd00bc9ee95cae81cd07
Adelino Penedo <agpene@hotmail.com>
2024-10-28 23:22:46 -0300

    fix: removed default controllers

--- backend_nestJS/src/app.module.ts
+++ backend_nestJS/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { LeadController } from "./lead/lead.controller";
+import { LeadService } from "./lead/lead.service";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [LeadController],
+  providers: [LeadService],
 })
 export class AppModule {}
