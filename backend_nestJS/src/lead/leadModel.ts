dd861a3c2ad420b585400102f438552b1b9beb35
Adelino Penedo <agpene@hotmail.com>
2024-10-28 23:24:54 -0300

    feat: models for the lead service

---
+++ backend_nestJS/src/lead/leadModel.ts
@@ -0,0 +1,35 @@
+export interface Lead {
+  id: string;
+  nomeCompleto: string;
+  email: string;
+  telefone: string;
+  unidades: Unidade[];
+}
+
+export interface Unidade {
+  id: string;
+  codigoDaUnidadeConsumidora: string;
+  modeloFasico: "monofasico" | "bifasico" | "trifasico";
+  enquadramento: "AX" | "B1" | "B2" | "B3";
+  historicoDeConsumoEmKWH: Consumo[];
+}
+
+export interface Consumo {
+  consumoForaPontaEmKWH: number;
+  mesDoConsumo: Date;
+}
+
+export interface LeadPDF {
+  valor: number;
+  barcode: string;
+  chargingModel: string;
+  phaseModel: string;
+  unit_key: string;
+  invoice: [invoicePDF];
+  energy_company_id: string;
+}
+
+export interface invoicePDF {
+  consumo_fp: number;
+  consumo_date: Date;
+}
