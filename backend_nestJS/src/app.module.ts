c2767d487b425696efa51333ab33af07b3dc84e8
Adelino Penedo <agpene@hotmail.com>
2024-10-28 23:30:54 -0300

    fix: corrects an error with the moduleDeclaration

--- backend_nestJS/src/app.module.ts
+++ backend_nestJS/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { LeadController } from "./lead/lead.controller";
 import { LeadService } from "./lead/lead.service";
+import { HttpModule } from "@nestjs/axios";
+import { PrismaService } from "./database/prisma.service";
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [LeadController],
-  providers: [LeadService],
+  providers: [LeadService, PrismaService],
 })
 export class AppModule {}
