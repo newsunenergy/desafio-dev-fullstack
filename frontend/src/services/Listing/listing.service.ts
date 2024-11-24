8fea5b94769e96a00b7d99594bf6e13d1dc761b5
matheusleite01 <de.leite2014@gmail.com>
2024-11-23 23:56:41 -0300

    feat(services/listing): add listing service

---
+++ frontend/src/services/Listing/listing.service.ts
@@ -0,0 +1,43 @@
+import { useMemo, useState } from "react";
+import { debounce } from "lodash";
+import { useQuery } from "@tanstack/react-query";
+import { Filter } from "@/components/modules/Listing/FilterSimulation/Typing";
+import { Lead } from "@/types/lead";
+
+import apiClient from "@/src/services/api/apiClient.service";
+
+export const useListingService = () => {
+  const [filterLeads, setFilterLeads] = useState({
+    name: "",
+    email: "",
+    code: "",
+  });
+  const [debouncedFilter, setDebouncedFilter] = useState(filterLeads);
+
+  const debouncedUpdate = useMemo(
+    () =>
+      debounce((newFilter) => {
+        setDebouncedFilter(newFilter);
+      }, 1000),
+    [],
+  );
+
+  const handleFilterChange = (newFilter: Filter) => {
+    setFilterLeads(newFilter);
+    debouncedUpdate(newFilter);
+  };
+
+  const { data: leads, isLoading } = useQuery({
+    queryKey: ["leads", debouncedFilter],
+    queryFn: async () => {
+      const response = await apiClient.get(
+        `/lead?nome=${filterLeads.name}&email=${filterLeads.email}&codigoDaUnidadeConsumidora=${filterLeads.code}`,
+      );
+      return response.data as Lead[];
+    },
+    enabled: Boolean(debouncedFilter),
+  });
+  return { filterLeads, setFilterLeads, leads, isLoading, handleFilterChange };
+};
+
+export default useListingService;
