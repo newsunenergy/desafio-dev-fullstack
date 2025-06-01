3250888ebb87f59c44d841e4ba49670fa039bd92
Mirian Quispe <mirianquispe1505@gmail.com>
2025-05-31 21:27:14 -0300

    fix: enable CORS with specific origin configuration in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3001);
 }
