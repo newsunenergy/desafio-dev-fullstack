54e4745fab0641a3ad1100eb4f9ef9b1ad66eca8
Carlos Dourado <carlosismaelad@outlook.com>
2025-12-03 11:45:34 -0300

    feat: add env.validation.ts to validate environment variable

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LeadsModule } from './leads/leads.module';
 import { PdfModule } from './pdf/pdf.module';
+import { validate } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      validate,
       isGlobal: true,
     }),
     LeadsModule,
